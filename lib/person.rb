ca37e068e703b45b1272c4d9f5a2f817a587edea
skyv26 <dev.aakashv@gmail.com>
2022-11-10 19:54:05 +0530

    Resolve `linters` error

--- lib/person.rb
+++ lib/person.rb
@@ -2,15 +2,12 @@ require 'securerandom'
 
 class Person
   attr_accessor :id, :name, :age, :parent_permission
-  attr_reader :is_of_age
-  alias is_of_age? of_age?
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = SecureRandom.hex(5)
     @name = name
     @age = age
     @parent_permission = parent_permission
-    @is_of_age = is_of_age?
   end
 
   def can_use_services?
