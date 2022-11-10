c548937f6730749798810f6caef5328d5b9fe78d
skyv26 <dev.aakashv@gmail.com>
2022-11-11 01:43:45 +0530

    Create wrapper file for decorator

--- decorators/decorator.rb
+++ decorators/decorator.rb
@@ -1,11 +1,3 @@
-require_relative '../interface/nameable'
-
-class Decorator < Nameable
-  def initialize(nameable_object)
-    @nameable = nameable_object
-  end
-
-  def correct_name
-    @nameable.correct_name
-  end
-end
+require_relative './base_decorator'
+require_relative './trimmer_decorator'
+require_relative './capitalize_decorator'
