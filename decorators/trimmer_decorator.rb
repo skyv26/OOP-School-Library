1e7c16292415490da8ce524a095e5476fa297961
skyv26 <dev.aakashv@gmail.com>
2022-11-11 01:44:28 +0530

    change the imported file name

--- decorators/trimmer_decorator.rb
+++ decorators/trimmer_decorator.rb
@@ -1,10 +1,9 @@
-require_relative './decorator'
+require_relative './base_decorator'
 
 class TrimmerDecorator < Decorator
   def correct_name
     if @nameable.correct_name.length > 10
       return @nameable.correct_name[0...10]
     end
-    @nameable.correct_name
   end
 end
