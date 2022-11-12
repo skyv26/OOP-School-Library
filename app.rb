6e1bbc499a4a9f13ecd714e453868d92d4f80e66
skyv26 <dev.aakashv@gmail.com>
2022-11-13 00:25:40 +0530

    implement the student and teacher data

--- app.rb
+++ app.rb
@@ -1,10 +1,7 @@
-require './lib/person'
+require './lib/student'
+require './lib/teacher'
 
 $basicUI = <<-DOC
-***********************************************
-Welcome to OOP School Library Management System
-***********************************************
-
 Please choose an option by entering a number:
 
 1 - List all books
@@ -20,18 +17,53 @@ DOC
 class App
 
   def initialize
-    @returnCheck = true
+    @person_List = []
+    @return_Check = true
   end
 
   def choice(num)
     case num
     when 1
-      true
+      puts
+      if @person_List.empty?
+        print "\nPlease first add some person\n"
+        true
+      else
+        @person_List.each_with_index do |person, index|
+          puts "#{index + 1} [#{person.class.name}] => ID: #{person.id}, Name: #{person.name}, Age: #{person.age}"
+        end
+      end
     when 2
       true
     when 3
       print 'Do you want to create a student (1) or a teacher (2) ? [Input the number] : '
       personChoice = gets.to_i
+      if(personChoice == 1)
+        print 'Age : '
+        age = gets.chomp
+        print 'Classroom : '
+        classroom = gets.chomp
+        print 'Name : '
+        name = gets.chomp
+        print 'Has parent permission ? [Y/N] : '
+        parent_permission = gets.chomp
+        if(parent_permission.downcase == 'y' || parent_permission == 'Y')
+          parent_permission = true
+        elsif(parent_permission.downcase == 'n' || parent_permission == 'N')
+          parent_permission = false
+        end
+      @person_List << Student.new(age, classroom, name: name, parent_permission: parent_permission)
+      elsif(personChoice == 2)
+        print 'Age : '
+        age = gets.chomp
+        print 'Name : '
+        name = gets.chomp
+        print 'Specialization : '
+        specialization = gets.chomp
+        @person_List << Teacher.new(age, specialization, name: name)
+      else
+        return true
+      end
     when 4
       true
     when 5
@@ -39,7 +71,7 @@ class App
     when 6
       true
     when 7
-      true
+      exit
     else
       true
     end
@@ -48,11 +80,11 @@ class App
 
   def run
     puts
-    while @returnCheck
+    while @return_Check
       print $basicUI
       print 'Enter your choice : '
       choice = gets.to_i
-      @returnCheck = self.choice(choice)
+      @return_Check = self.choice(choice)
       puts
     end
   end
