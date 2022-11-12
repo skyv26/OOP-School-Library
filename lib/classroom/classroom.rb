cda71863e5e20e0a283f68982446787e3ae41a49
skyv26 <dev.aakashv@gmail.com>
2022-11-12 15:05:26 +0530

    add the `has-many` and `belongs-to` relationships

--- lib/classroom/classroom.rb
+++ lib/classroom/classroom.rb
@@ -1,7 +1,14 @@
 class Classroom
   attr_accessor :label
+  attr_reader :student
 
   def initialize(label)
     @label = label
+    @student = []
+  end
+
+  def add(student)
+    student.classroom = self
+    @student << student
   end
 end
