0047f39cd6c676886414291de5dccf0273a758e0
skyv26 <dev.aakashv@gmail.com>
2022-11-13 00:44:39 +0530

    Resolved the linters error

--- app.rb
+++ app.rb
@@ -1,90 +1,90 @@
 require './lib/student'
 require './lib/teacher'
 
-$basicUI = <<-DOC
-Please choose an option by entering a number:
-
-1 - List all books
-2 - List all people
-3 - Create a person
-4 - Create a book
-5 - Create a rental
-6 - List all rentals for a given person id
-7 - Exit
-
-DOC
-
 class App
-
   def initialize
-    @person_List = []
-    @return_Check = true
+    @person_list = []
+    @return_check = true
+  end
+
+  def create_student
+    print 'Age : '
+    age = gets.chomp
+    print 'Classroom : '
+    classroom = gets.chomp
+    print 'Name : '
+    name = gets.chomp
+    print 'Has parent permission ? [Y/N] : '
+    parent_permission = gets.chomp
+    if parent_permission.downcase == 'y' || parent_permission == 'Y'
+      parent_permission = true
+    elsif parent_permission.downcase == 'n' || parent_permission == 'N'
+      parent_permission = false
+    end
+    @person_list << Student.new(age, classroom, name: name, parent_permission: parent_permission)
+  end
+
+  def create_teacher
+    print 'Age : '
+    age = gets.chomp
+    print 'Name : '
+    name = gets.chomp
+    print 'Specialization : '
+    specialization = gets.chomp
+    @person_list << Teacher.new(age, specialization, name: name)
   end
 
   def choice(num)
     case num
     when 1
       puts
-      if @person_List.empty?
+      if @person_list.empty?
         print "\nPlease first add some person\n"
-        true
       else
-        @person_List.each_with_index do |person, index|
+        @person_list.each_with_index do |person, index|
           puts "#{index + 1} [#{person.class.name}] => ID: #{person.id}, Name: #{person.name}, Age: #{person.age}"
         end
       end
-    when 2
-      true
     when 3
-      print 'Do you want to create a student (1) or a teacher (2) ? [Input the number] : '
-      personChoice = gets.to_i
-      if(personChoice == 1)
-        print 'Age : '
-        age = gets.chomp
-        print 'Classroom : '
-        classroom = gets.chomp
-        print 'Name : '
-        name = gets.chomp
-        print 'Has parent permission ? [Y/N] : '
-        parent_permission = gets.chomp
-        if(parent_permission.downcase == 'y' || parent_permission == 'Y')
-          parent_permission = true
-        elsif(parent_permission.downcase == 'n' || parent_permission == 'N')
-          parent_permission = false
-        end
-      @person_List << Student.new(age, classroom, name: name, parent_permission: parent_permission)
-      elsif(personChoice == 2)
-        print 'Age : '
-        age = gets.chomp
-        print 'Name : '
-        name = gets.chomp
-        print 'Specialization : '
-        specialization = gets.chomp
-        @person_List << Teacher.new(age, specialization, name: name)
-      else
-        return true
-      end
-    when 4
-      true
-    when 5
-      true
-    when 6
+      person_choice?
       true
     when 7
       exit
+    end
+  end
+
+  def person_choice?
+    print 'Do you want to create a student (1) or a teacher (2) ? [Input the number] : '
+    person_choice = gets.to_i
+    case person_choice
+    when 1
+      create_student
+    when 2
+      create_teacher
     else
       true
     end
-
+    puts 'Person Created Successfully !!'
   end
 
   def run
     puts
-    while @return_Check
-      print $basicUI
+    while @return_check
+      print <<~DOC
+        Please choose an option by entering a number:
+
+        1 - List all books
+        2 - List all people
+        3 - Create a person
+        4 - Create a book
+        5 - Create a rental
+        6 - List all rentals for a given person id
+        7 - Exit
+
+      DOC
       print 'Enter your choice : '
       choice = gets.to_i
-      @return_Check = self.choice(choice)
+      @return_check = self.choice(choice)
       puts
     end
   end
--- main.rb
+++ main.rb
@@ -6,9 +6,9 @@ def main
   app.run
 end
 
-print <<-DOC
-***********************************************
-Welcome to OOP School Library Management System
-***********************************************
+print <<~DOC
+  ***********************************************
+  Welcome to OOP School Library Management System
+  ***********************************************
 DOC
 main
