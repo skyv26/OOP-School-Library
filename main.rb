4b38080385bd3829030d4c2c8e0ab45ff15d54b3
skyv26 <dev.aakashv@gmail.com>
2022-11-12 22:36:44 +0530

    create the executable ready file

--- main.rb
+++ main.rb
@@ -1,11 +1,9 @@
-require_relative './lib/person'
-require_relative './decorators/decorator'
+#!/usr/bin/env ruby
+require_relative 'app'
 
-person = Person.new(22, 'maximilianus')
-puts person.correct_name
+def main
+  app = App.new
+  app.run
+end
 
-capitalized_person = CapitalizeDecorator.new(person)
-puts capitalized_person.correct_name
-
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts capitalized_trimmed_person.correct_name
+main
